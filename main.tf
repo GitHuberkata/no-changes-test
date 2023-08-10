931184237f83a36b82e36cf843d829797303c52d
GitHuberkata <petyagavrilova86@gmail.com>
2023-08-10 16:53:50 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.53.0"
+    }
+  }
+}
+
+resource "null_resource" "screen_output" {
+count = 21000
+  provisioner "local-exec" {
+    command = <<EOT
+    echo 'Hello'
+    EOT
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-06e85d4c3149db26a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "westVM"
+  }
+}
